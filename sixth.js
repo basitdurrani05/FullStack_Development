64a300cdcfda89555d35643e0decd8c51ba7e9fe
basitdurrani <basitdurrani05786@gmail.com>
2025-03-24 21:55:13 +0530

    Second Commit

---
+++ sixth.js
@@ -0,0 +1,48 @@
+// let a = document.getElementById('first');
+// a.style.color = 'red';
+
+
+// Browser Object Model (BOM)
+
+// alert("It's a 18+ Contents , you are not enter because you are a child......")
+// confirm("Are you 18 + ?");
+// prompt("Enter Your Age : ");    Taking User Input
+
+
+
+// DOM Nodes
+
+// 1). text node
+// 2). element node
+// 3). Comment node
+
+
+
+// What is Searching DOM OR How many type of selector in javascript.
+
+// let a = document.getElementById();
+// let b = document.getElementsByClassName();
+// let c= document.querySelector();
+// let d = document.querySelectorAll();
+
+
+
+
+
+// let a = document.getElementById('first');
+
+// let b = document.getElementsByClassName('first');
+
+// let a = document.querySelector('#first')
+// let a = document.querySelectorAll('#first');
+
+// for(let i = 0; i < a.length; i++){
+
+//     a[i].style.color = 'red';
+// }
+
+
+
+
+
+
